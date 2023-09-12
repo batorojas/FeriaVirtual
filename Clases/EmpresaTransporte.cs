ed1a9c22bec36b5a1f96da5a8464a1ab8811819c
Ioannis Obreque <ioannis.isob@gmail.com>
2023-09-11 23:45:23 -0300

    ingresar transporte & eliminar usuario perfil admin

---
+++ Clases/EmpresaTransporte.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class EmpresaTransporte
+    {
+        private int idEmpresaTransporte;
+        private String nombreEmpresaTransporte;
+        private int idUsuarioEmpresaTransporte;
+
+        public int IdEmpresaTransporte { get => idEmpresaTransporte; set => idEmpresaTransporte = value; }
+        public string NombreEmpresaTransporte { get => nombreEmpresaTransporte; set => nombreEmpresaTransporte = value; }
+        public int IdUsuarioEmpresaTransporte { get => idUsuarioEmpresaTransporte; set => idUsuarioEmpresaTransporte = value; }
+    }
+}
